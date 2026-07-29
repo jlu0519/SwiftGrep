9f948a8bae6e5d747b4fe94dce4425256a16157c
Luke Young <lukeyoung0519@gmail.com>
2026-07-29 06:39:41 -0700

    Improve parseFlags termination logic

--- main.cpp
+++ main.cpp
@@ -26,72 +26,76 @@ SetFlags parseFlags(const std::vector<std::string>& commandArguments)
 {
     // Parse consecutive flags at the beginning of the command.
     SetFlags userFlags;
-    int argc = static_cast<int>(commandArguments.size());
+    int numberOfArguments = static_cast<int>(commandArguments.size());
     int lengthOfArgument {};
 
-    for( int i = 1; i < argc; ++i)
+    for( int i = 1; i < numberOfArguments; ++i)
     { 
-        if(commandArguments[i] == "--")
+        if(!commandArguments[i].empty())
         {
-            userFlags.endOfOptions = true;
-            ++userFlags.flagCommandArguments;
-            return userFlags;
-        }
-        else if (commandArguments[i][0] == '-')
-        {
-            lengthOfArgument = commandArguments[i].size();
-            
-            if(lengthOfArgument <= 1)
+            if(commandArguments[i] == "--")
             {
-                std::cerr << "Invalid Option: Hint: [-i] [-v] [-c] [-l] [-f] [-r]" << "\n";
-
-                
-                std::exit(0);
+                userFlags.endOfOptions = true;
+                ++userFlags.flagCommandArguments;
+                return userFlags;
             }
-
-            for(int j = 1; j < lengthOfArgument; ++j)
+            else if (commandArguments[i][0] == '-')
             {
-                if(commandArguments[i][j] == 'i')
-                {
-                    userFlags.caseInsensitive = true;
-                }
-                else if(commandArguments[i][j] == 'v')
-                {
-                    userFlags.invertMatch = true;
-                }
-                else if(commandArguments[i][j] == 'c')
-                {
-                    userFlags.countOnly = true;
-                }
-                else if(commandArguments[i][j] == 'l')
-                {
-                    userFlags.lineNumbers = true;
-                }
-                else if(commandArguments[i][j] == 'f')
-                {
-                    userFlags.showFile = true;
-                }
-                else if(commandArguments[i][j] == 'r')
-                {
-                    userFlags.recursiveSearch = true;
-                    userFlags.showFile= true;
-                }
-                else
+                lengthOfArgument = commandArguments[i].size();
+                
+                if(lengthOfArgument <= 1)
                 {
                     std::cerr << "Invalid Option: Hint: [-i] [-v] [-c] [-l] [-f] [-r]" << "\n";
-                    std::exit(0);
+                    std::exit(2);
                 }
+
+                for(int j = 1; j < lengthOfArgument; ++j)
+                {
+                    if(commandArguments[i][j] == 'i')
+                    {
+                        userFlags.caseInsensitive = true;
+                    }
+                    else if(commandArguments[i][j] == 'v')
+                    {
+                        userFlags.invertMatch = true;
+                    }
+                    else if(commandArguments[i][j] == 'c')
+                    {
+                        userFlags.countOnly = true;
+                    }
+                    else if(commandArguments[i][j] == 'l')
+                    {
+                        userFlags.lineNumbers = true;
+                    }
+                    else if(commandArguments[i][j] == 'f')
+                    {
+                        userFlags.showFile = true;
+                    }
+                    else if(commandArguments[i][j] == 'r')
+                    {
+                        userFlags.recursiveSearch = true;
+                        userFlags.showFile= true;
+                    }
+                    else
+                    {
+                        std::cerr << "Invalid Option: Hint: [-i] [-v] [-c] [-l] [-f] [-r]" << "\n";
+                        std::exit(2);
+                    }
+                }
+
+            }
+            else
+            {
+                return userFlags;
             }
 
+            ++userFlags.flagCommandArguments;
         }
         else
         {
             return userFlags;
         }
-
-        ++userFlags.flagCommandArguments;
     }
-
     return userFlags;
 }
 
